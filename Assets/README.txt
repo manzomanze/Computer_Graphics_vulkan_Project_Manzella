12e4fde71c89beb9138ec37690687d9b0ce640b5
manzomanze <manzella.davide97@gmail.com>
2022-07-07 16:46:59 +0200

    first setup

---
+++ Assets/README.txt
@@ -0,0 +1,12 @@
+Here you have all the pieces of the pinball, either divided one by one
+or all together in the Pinball.blend or PinballDark.blend files.
+I left all the .blend files so you can modify them if you need.
+The objPositions.js file holds the local matrices of all the single pieces with
+respect to the body.
+Beware that rotations are tricky with these models so I suggest you create an example
+scene to see how each object behaves with simple rotations.
+You can choose to use the light version or be seduced by the dark side.
+They all use the same texture in the root folder.
+There are also the digits if you want to implement a dynamic scoring system.
+Use the positions given in the js file to place them in the 3D environment 
+but choose the digit that is uv mapped to the number you need.
---
+++ Assets/objPositions.js
@@ -0,0 +1,49 @@
+var ballLocalMatrix = utils.MakeWorld(-0.30053, 8.5335, -5.9728, 0.0, 0.0, 0.0, 1.0);
+
+var dl1LocalMatrix = utils.MakeWorld(0.4366, 12.789, 4.1852, 0.0, -101.0, 0.0, 1.0);
+
+
+
+
+
+
+
+
+
+var dl2LocalMatrix = utils.MakeWorld(0.713, 12.789, 4.1852, 0.0, -101.0, 0.0, 1.0);
+
+var dl3LocalMatrix = utils.MakeWorld(0.9923, 12.789, 4.1852, 0.0, -101.0, 0.0, 1.0);
+
+var dl4LocalMatrix = utils.MakeWorld(1.3917, 12.789, 4.1852, 0.0, -101.0, 0.0, 1.0);
+
+var dl5LocalMatrix = utils.MakeWorld(1.6681, 12.789, 4.1852, 0.0, -101.0, 0.0, 1.0);
+
+var dl6LocalMatrix = utils.MakeWorld(1.9474, 12.789, 4.1852, 0.0, -101.0, 0.0, 1.0);
+
+var dr1LocalMatrix = utils.MakeWorld(-2.8273, 12.789, 4.1852, 0.0, -101.0, 0.0, 1.0);
+
+var dr2LocalMatrix = utils.MakeWorld(-2.5509, 12.789, 4.1852, 0.0, -101.0, 0.0, 1.0);
+
+var dr3LocalMatrix = utils.MakeWorld(-2.2716, 12.789, 4.1852, 0.0, -101.0, 0.0, 1.0);
+
+var dr4LocalMatrix = utils.MakeWorld(-1.8722, 12.789, 4.1852, 0.0, -101.0, 0.0, 1.0);
+
+var dr5LocalMatrix = utils.MakeWorld(-1.5958, 12.789, 4.1852, 0.0, -101.0, 0.0, 1.0);
+
+var dr6LocalMatrix = utils.MakeWorld(-1.316, 12.789, 4.1852, 0.0, -101.0, 0.0, 1.0);
+
+var leftFlipperLocalMatrix = utils.MakeWorld(0.6906, 8.4032, -5.6357,29.8,-3.24,-5.64, 1.0);
+
+var rightFlipperLocalMatrix = utils.MakeWorld(-1.307, 8.4032, -5.6357, 150.0,-3.24,-5.64, 1.0);
+
+var leftButtonLocalMatrix = utils.MakeWorld(2.6175, 8.7853, -6.6902, 0.0, 0.0, -90.0, 1.0);
+
+var rightButtonLocalMatrix = utils.MakeWorld(-2.97, 8.7853, -6.6902, 0.0, 0.0, 90.0, 1.0);
+
+var pullerLocalMatrix = utils.MakeWorld(-2.5264, 8.3925, -7.5892, 0.0, -90.0, 0.0, 1.0);
+
+var bumper1LocalMatrix = utils.MakeWorld(1.1819, 9.1362, 0.020626, -6.51, 0.0, 0.0, 1.0);
+
+var bumper2LocalMatrix = utils.MakeWorld(-1.5055, 9.1362, 0.020626, -6.51, 0.0, 0.0, 1.0);
+
+var bumper3LocalMatrix = utils.MakeWorld(-0.11626, 9.1362, 0.020626, -6.51, 0.0, 0.0, 1.0);
